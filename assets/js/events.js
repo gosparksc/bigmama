3c03dd2404940c31e4582c41ff34bda63366a2e0
CalvinLeGassick <calvin.legassick@gmail.com>
2015-07-30 02:43:28 -0700

    Adds in javascript for animating cards up

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,21 +1,28 @@
-$.ajax({
-  url: "http://localhost:1111/fragments/1000-pitches.html",
-  dataType: "html",
-  success: function(response) {
-    $('.event-info').css("display", "block");
+$(".event").each(function() {
+    $(this).click(function() {
+        console.log("HERE!");
+        var event_name = $(this).attr('id').split("/")[2];
+        console.log(event_name);
+        $.ajax({
+            url: "http://localhost:1111/fragments/"+event_name,
+            dataType: "html",
+            success: function(response) {
+                $('.event-info').css("display", "block");
 
-    // Load fragment into event page div and get container height
-    var event_page = $('#event-page').html(response);
-    var height = $('.container').height();
+                // Load fragment into event page div and get container height
+                var event_page = $('#event-page').html(response);
+                var height = $('.container').height();
 
-    // Move event page to bottom of page
-    event_page.offset({
-        top: height
+                // Move event page to bottom of page
+                event_page.offset({
+                    top: height
+                });
+
+                // Animate the event page back to the top
+                event_page.delay(2000).animate({
+                    top: 0
+                }, 1200);
+            }
+        });
     });
-
-    // Animate the event page back to the top
-    event_page.delay(2000).animate({
-        top: 0
-    }, 1200);
-  }
 });
