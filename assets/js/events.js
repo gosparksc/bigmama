86593f207d97107e9e16d6223b9e081187a65d87
CalvinLeGassick <calvin.legassick@gmail.com>
2015-07-30 02:09:35 -0700

    Adds fragments folder and animation for fragment loading, tested in an ajax call

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,7 +1,21 @@
-// $.ajax({
-//   url: "events/1000-pitches.html",
-//   dataType: "html",
-//   success: function(response) {
-//     $('#event-page').html(response);
-//   }
-// });
+$.ajax({
+  url: "http://localhost:1111/fragments/1000-pitches.html",
+  dataType: "html",
+  success: function(response) {
+    $('.event-info').css("display", "block");
+
+    // Load fragment into event page div and get container height
+    var event_page = $('#event-page').html(response);
+    var height = $('.container').height();
+
+    // Move event page to bottom of page
+    event_page.offset({
+        top: height
+    });
+
+    // Animate the event page back to the top
+    event_page.delay(2000).animate({
+        top: 0
+    }, 1200);
+  }
+});
