a4692ce5139c6ed9b95c259e74184fcb46670cbd
CalvinLeGassick <calvin.legassick@gmail.com>
2015-08-04 02:43:26 -0700

    Adds correct routing behavior for events page and starts to fill in individual event info

--- assets/js/events.js
+++ assets/js/events.js
@@ -7,6 +7,9 @@ $(".event").each(function() {
             success: function(response) {
                 // Load fragment into event page div and get container height
                 var event_page = $('#event-page').append(response);
+                var event_info = $('.event-info');
+                event_info.css("background:rgba(0,0,0,0)");
+
                 var height = $('.container').height();
 
                // Move event page to bottom of page
@@ -15,10 +18,61 @@ $(".event").each(function() {
                 });
 
                 // Animate the event page back to the top
-                event_page.delay(700).animate({
+                event_page.animate({
                     top: 0
-                }, 1200);
+                }, 800, function() {
+                    // Change browser history after animation completes
+                    window.history.pushState({page: window.location.href}, 'events/'+event_name.split(".")[0], '/events/'+event_name.split(".")[0]);
+                });
+                
             }
+
         });
+
     });
+
+});
+
+// Handles forwards and backwards on this page
+$(window).on('popstate', function(e) {
+
+    var location = e.originalEvent.currentTarget.location.href;
+    var location_split = location.split("/");
+
+    // Clear event page div on "back"
+    if(location_split.length == 4) {
+        // HACK:
+        // Popstate gets called multiple times on forward/backward
+        // Need to check if html is empty before trying to store fragment.
+        _html = $("#event-page").html();
+        if(_html.length > 0) {
+            window.sessionStorage.fragment = _html;
+        }
+        $('#event-page').html("");
+
+    // Set event information of "forward"
+    } else if(location_split.length == 5) {
+        _html = window.sessionStorage.fragment;
+        if(_html.length > 0) {
+            $('#event-page').html(_html);
+        } else {
+            event_name = location_split[4];
+           $.ajax({
+                url: "http://localhost:1111/fragments/"+event_name,
+                dataType: "html",
+                success: function(response) {
+                // Load fragment into event page div and get container height
+                    var event_page = $('#event-page').append(response); 
+                }
+            });
+
+        }
+
+    }
+
+});
+
+// Used to avoid ajax request on 'x'
+$(".event-page-close").click( function() {
+    $('#event-page').html("");
 });
