503cdaf83e439b4ee37390d00abc1a50444d0729
CalvinLeGassick <calvin.legassick@gmail.com>
2015-08-13 13:49:24 -0700

    Timing thing for le gef

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ src = '/video/spark.gif';
 img.src=src;
 setInterval(function(){
     t=new Date().getTime();
-    $("#gif").attr("src", src);
+    $("#gif").attr("src", src + "?" + t);
 }, 7500);
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
